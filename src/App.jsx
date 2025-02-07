4e39df9209daefa6931ba3695ed78d7e480573d6
Shereena <shereenaruby94@gmail.com>
2025-02-07 13:29:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import { Route, Routes } from 'react-router-dom' 
+import './App.css'
+import Content from './Pages/Content'
+import Home from './Pages/Home'
+import History from './Pages/History'
+import Header from './Pages/Header'
+import Footer from './Pages/Footer'
+
+
+
+
+function App() {
+
+  return (
+    <>
+    < Header/>
+    <Routes>
+      <Route element={< Content/>} path = '/'></Route>
+      <Route element={< Home/>} path= '/home'></Route>
+      <Route element={< History/>} path= '/history'></Route>
+    </Routes>
+
+    < Footer/>
+
+    
+     
+    </>
+  )
+}
+
+export default App
